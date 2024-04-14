2090e6675a107ceebbb11a88291018e665c5f20c
mzurera- <mzurera-@student.42malaga.com>
2024-04-14 19:43:54 +0200

    Removed file naming test.

--- Python/Two-Sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        diff = {}
-        for i, n in enumerate(nums):
-            if n in diff:
-                return (diff[n], i)
-            diff[target - n] = i
-        return (-1, -1)
